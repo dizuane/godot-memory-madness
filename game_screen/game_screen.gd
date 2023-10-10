f28b7276373140e22814b3e68f3685effcabf702
Duane Spainhour <duane.spainhour@gmail.com>
2023-10-10 01:51:58 -0400

    Created tile prefab, logic for selecting pairs of images and grid setup

--- game_screen/game_screen.gd
+++ game_screen/game_screen.gd
@@ -1,12 +1,29 @@
 extends Control
 
+@export var mem_tile_scene: PackedScene
+
 @onready var sound = $Sound
+@onready var tile_container = $HB/MC1/TileContainer
 
 func _ready():
-	pass # Replace with function body.
+	SignalManager.on_level_selected.connect(on_level_selected)
 
 func _process(delta):
 	pass
+
+func add_memory_tile(ii_dict: Dictionary, fi: CompressedTexture2D) -> void:
+	var new_tile = mem_tile_scene.instantiate()
+	tile_container.add_child(new_tile)
+	new_tile.setup(ii_dict, fi)
+	
+func on_level_selected(level_num: int) -> void:
+	var level_selection = GameManager.get_level_selection(level_num)
+	var frame_image = ImageManager.get_random_frame_image()
+	
+	tile_container.columns = level_selection.num_cols
+	
+	for ii_dict in level_selection.image_list:
+		add_memory_tile(ii_dict, frame_image)
 
 func _on_exit_button_pressed():
 	SoundManager.play_button_click(sound)
